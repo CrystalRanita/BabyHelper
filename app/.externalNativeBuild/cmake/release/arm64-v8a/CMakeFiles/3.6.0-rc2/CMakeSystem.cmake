d136bbd176149f13c5d8ad71e26ac6c17ef11f2b
crystal.lin <crystal.lin@acer.com>
2018-07-03 08:08:05 +0800

    Add OpenCV

--- app/.externalNativeBuild/cmake/release/arm64-v8a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/release/arm64-v8a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,13 +1,13 @@
-set(CMAKE_HOST_SYSTEM "Linux-4.13.0-36-generic")
+set(CMAKE_HOST_SYSTEM "Linux-4.13.0-41-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "4.13.0-36-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "4.13.0-41-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/home/crystal/sdk/android-ndk-r15c/build/cmake/android.toolchain.cmake")
+include("/home/crystal/sdk/android_ndk/build/cmake/android.toolchain.cmake")
 
-set(CMAKE_SYSTEM "Android-21")
+set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
-set(CMAKE_SYSTEM_VERSION "21")
+set(CMAKE_SYSTEM_VERSION "1")
 set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 
 set(CMAKE_CROSSCOMPILING "TRUE")
